9246ca17e56741138b79c9ed5d4ac4e1dceba51a
Eugene <zhenyaskryaga@gmail.com>
2026-06-02 23:05:38 +0300

    homework 57

---
+++ src/services/tasks.js
@@ -0,0 +1,90 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const DB_PATH = path.resolve("src", "db", "tasks.json");
+
+const readTasks = async () => {
+  const tasks = await fs.readFile(DB_PATH, "utf-8");
+  return JSON.parse(tasks);
+};
+
+const writeTasks = async tasks => {
+  await fs.writeFile(DB_PATH, JSON.stringify(tasks, null, 2), "utf-8");
+};
+
+export const getAllTasks = async () => {
+  const tasks = await readTasks();
+  console.log(tasks);
+};
+
+export const getTaskById = async id => {
+  const tasks = await readTasks();
+  const task = tasks.find(task => task.id === Number(id));
+
+  if (!task) {
+    throw new Error("Task not found");
+  }
+
+  console.log(task);
+};
+
+export const addTask = async title => {
+  if (!title) {
+    throw new Error("Title is required");
+  }
+
+  const tasks = await readTasks();
+  const newTask = {
+    id: Date.now(),
+    title,
+    completed: false,
+  };
+
+  tasks.push(newTask);
+
+  await writeTasks(tasks);
+
+  console.log(newTask);
+};
+
+export const updateTasks = async ({ id, title, completed }) => {
+  if (!id) {
+    throw new Error("ID is required");
+  }
+
+  const tasks = await readTasks();
+  const taskIndex = tasks.findIndex(task => task.id === Number(id));
+
+  if (taskIndex === -1) {
+    throw new Error("Task not found");
+  }
+
+  if (title) {
+    tasks[taskIndex].title = title;
+  }
+
+  if (completed !== undefined) {
+    tasks[taskIndex].completed = completed === "true";
+  }
+
+  await writeTasks(tasks);
+  console.log(tasks[taskIndex]);
+};
+
+export const removeTask = async id => {
+  if (!id) {
+    throw new Error("ID is required");
+  }
+
+  const tasks = await readTasks();
+  const taskExists = tasks.some(task => task.id === Number(id));
+
+  if (!taskExists) {
+    throw new Error("Task not found");
+  }
+
+  const filteredTasks = tasks.filter(task => task.id !== Number(id));
+
+  await writeTasks(filteredTasks);
+  console.log(`Task with ID ${id} deleted`);
+};
